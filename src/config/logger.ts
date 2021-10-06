07bfe3e452fd921e1f8642d858e867fd23a3ec09
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2021-10-07 00:14:41 +0300

    Update logger

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -4,27 +4,28 @@ const getTimeStamp = (): string => {
     return new Date().toISOString();
 };
 
-const logger = (req: Request, res: Response, logType: string, isRes: boolean = false): void => {
+class Logger {
+    private static status_text = ', REQUEST';
+    private static warn_color_code = '\x1b[33m%s\x1b[0m';
+    private static error_color_code = '\x1b[31m%s\x1b[0m';
 
-    // Logger function
-    let 
-        status_text = ', REQUEST', 
-        warn_color_code = '\x1b[33m%s\x1b[0m',
-        error_color_code = '\x1b[31m%s\x1b[0m';
+    constructor() {}
 
-    if (isRes) {
-        status_text = `, STATUS - [${res.statusCode}]`;
+    log(req: Request, res: Response, logType: string, isRes: boolean = false): void {
+        if (isRes) {
+            Logger.status_text = `, STATUS - [${res.statusCode}]`;
+        }
+
+        switch(logType) {
+            case 'i': 
+                console.log(`[${getTimeStamp()}] [INFO], HOST-  [${req.hostname}], METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}]${Logger.status_text}`); break;
+            case 'w':
+                console.warn(Logger.warn_color_code, `[${getTimeStamp()}] [WARNING], HOST-  [${req.hostname}] METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}]${Logger.status_text}`); break;
+            case 'e':
+                console.error(Logger.error_color_code, `[${getTimeStamp()}] [ERROR], HOST-  [${req.hostname}] METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}]${Logger.status_text}`); break;
+            default: break;
+        }
     }
+}
 
-    switch(logType) {
-        case 'i': 
-            console.log(`[${getTimeStamp()}] [INFO], HOST-  [${req.hostname}], METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}]${status_text}`); break;
-        case 'w':
-            console.warn(warn_color_code, `[${getTimeStamp()}] [WARNING], HOST-  [${req.hostname}] METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}]${status_text}`); break;
-        case 'e':
-            console.error(error_color_code, `[${getTimeStamp()}] [ERROR], HOST-  [${req.hostname}] METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}]${status_text}`); break;
-        default: break;
-    }
-};
-
-export default logger;
+export default new Logger().log;
